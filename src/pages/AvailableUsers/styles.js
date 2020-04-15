a7f0e9344910a693d50f59092f005f2feccd5184
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2020-04-15 14:22:58 -0300

    Adicionando estilo para tela de listagem

---
+++ src/pages/AvailableUsers/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+	container: {
+    paddingTop: Constants.statusBarHeight + 20,
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#EC2041'
+	},
+});

91d3e90f0e93e23f2051992f1fd9f106f3fd6890
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-04-18 15:09:58 -0300

    Adiciona pin destino e tracando rota ate o mesmo utilizando api do google maps

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { Text, Button, View } from 'react-native';
-import MapView, {Marker, Callout} from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import styles from './styles';
 import mapStyleConfig from './mapStyleConfig';
 import * as Location from 'expo-location';
+import { getPixelSize } from '../../utils';
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyANApQdINHhAihyiBI67nVnSB3F2mn_Ugo';
 
 function Home({ navigation }) {
 	const deltaLatLong = {latitudeDelta: 0.0143, longitudeDelta: 0.0134};
+	const mapStyle = (new Date().getHours() > 19) ? mapStyleConfig : null;
+	const [destination, setDestination] = useState(false);
+	const [mapView, setMapView] = useState(false);
 
 	const [region, setRegion] = useState({
 		latitude: 0,
 		longitude: 0,
 		...deltaLatLong
 	});
-
-	const mapStyle = (new Date().getHours() > 19) ? mapStyleConfig : null;
 
 	useEffect(() => {
 		(async () => {
@@ -27,38 +32,56 @@ function Home({ navigation }) {
 			let {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
 			
 			setRegion({
-				latitude, longitude,
+				latitude, 
+				longitude,
 				...deltaLatLong
 			});
 		})();
 	}, []);
 
-	function onRegionChange(region) {
-		console.log(region)
-	}
 
-	function onRegionPress(event) {
-		console.log(event)
+	function handleDestinationSelected({ nativeEvent: { coordinate } }) {
+		const { latitude, longitude } = coordinate;
+		setDestination({ latitude, longitude });
 	}
 	
 	return(
 		<View style={styles.container}>
-			<MapView 
+			<MapView
+				provider={PROVIDER_GOOGLE}
 				style={styles.mapStyle}
 				region={region}
-				onRegionChange={onRegionChange}
-				onPress={onRegionPress}
 				showsUserLocation
 				loadingEnabled 
-				customMapStyle={mapStyle}>
+				customMapStyle={mapStyle}
+				ref={el => setMapView(el)}>
 				<Marker
 					coordinate={{
-						latitude: -23.4696183,
-						longitude: -46.539066,
+						latitude: -23.504908,
+						longitude: -46.556163,
 					}}
 					title={"Felipe Fanucchi"}
 					description={"Apartamento 83 C - Em frente a academia sanchez"}
+					onPress={handleDestinationSelected}
 				/>
+
+				{destination && (<MapViewDirections
+					origin={region}
+					destination={destination}
+					strokeWidth={3}
+					strokeColor="#EC2041"
+					apikey={GOOGLE_MAPS_APIKEY}
+					onReady={result => {
+						mapView.fitToCoordinates(result.coordinates, {
+							edgePadding: {
+								right: getPixelSize(80),
+								top: getPixelSize(80),
+								bottom: getPixelSize(80),
+								left: getPixelSize(80),
+							},
+						});
+					}}
+				/>)}
 			</MapView>
 		</View>
 	);
